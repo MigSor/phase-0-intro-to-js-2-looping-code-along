78bdcd2cee94d8898334985844134fb8868c51e3
Miguel Soriano <miguel.sor.88@gmail.com>
2023-07-11 17:44:07 +1000

    added writeCards and countDown function to past test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(arrName, evtName) {
+  let newArray = [];
+  for (let i = 0; i < arrName.length; i++) {
+    //"Thank you, Charlie, for the wonderful birthday gift!"
+    newArray.push(
+      `Thank you, ${arrName[i]}, for the wonderful ${evtName} gift!`
+    );
+  }
+  return newArray;
+}
+
+// console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(givenNum) {
+  while (givenNum >= 0) {
+    console.log(givenNum);
+    givenNum--;
+  }
+}
+
+// countDown(20);
